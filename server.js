30460e6de320e9796c07423992fd4774a84847bd
Danilo Trindade <chapolin@gmail.com>
2015-05-13 15:34:50 -0300

    Implements gallery test

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/time', function (req, res) {
 	res.render('team');
 });
 
+app.get('/fotos', function (req, res) {
+	res.render('fotos');
+});
+
 var server = app.listen(port, function() {
 	var host = server.address().address;
 	var port = server.address().port;
